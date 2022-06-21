8c7129471102b5f55b2680aa3ae653a4f982870f
devfrank9 <devfrank9@gmail.com>
2022-06-21 17:20:57 +0900

    ..

---
+++ server/controller/index.js
@@ -0,0 +1,49 @@
+const { agoraStatesDiscussions } = require("../repository/discussions");
+const data = agoraStatesDiscussions;
+
+const discussionsController = {
+  findAll: (req, res) => {
+    // TODO: 모든 discussions 목록을 응답합니다.
+    // ADVANCED: 테스트 케이스에 맞게 페이지네이션을 구현합니다.
+    return res.status(200).json(data);
+  },
+
+  findById: (req, res) => {
+    // TODO: path parameter id를 가진 discussion을 응답합니다.
+    const { id } = req.params;
+    if (req.params) {
+      data.some((item) => item.id === Number(id))
+        ? res.status(200).json(data.filter((item) => Number(id) === item.id)[0])
+        : res.status(404).json();
+    }
+  },
+
+  createOne: (req, res) => {
+    // ADVANCED: 새로운 discussion을 생성합니다.
+    data.push(req.body);
+    return res.status(201).json({});
+  },
+
+  updateById: (req, res) => {
+    // ADVANCED: path parameter id를 가진 discussion을 요청에 맞게 변경합니다.
+    const { id } = req.params;
+    if (req.params) {
+      data = data.filter((item) => Number(id) === item.id)[0];
+      data = { ...data, ...req.body };
+      return res.status(200).json(data);
+    }
+  },
+
+  deleteById: (req, res) => {
+    // ADVANCED: path parameter id를 가진 discussion을 삭제합니다.
+    const { id } = req.params;
+    if (id) {
+      data = data.filter((item) => Number(id) !== item.id);
+    }
+    return res.status(200).json(data);
+  },
+};
+
+module.exports = {
+  discussionsController,
+};
